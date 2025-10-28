67facea670f32c203c9d22a6b55e04e68a53aa88
brian okoth okuku <okukubrian743@gmail.com>
2025-10-29 01:45:45 +0300

    Remove placeholder code from __init__.py

--- backend/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-# Initialize app and database
-app = Flask(__name__)
-db = SQLAlchemy(app)
-
-# Import routes after app is created
-from app import routes
