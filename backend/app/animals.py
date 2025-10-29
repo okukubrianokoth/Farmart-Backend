1b75fa8c29023dc9e97e166d6a2be811d5ea8f42
HUSSEIN-IGM <HUSSEINNGUFU205@gmail.com>
2025-10-29 21:43:11 +0300

    animal bp

--- backend/app/animals.py
+++ backend/app/animals.py
@@ -3,3 +3,5 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from app import db
 from app.models import Animal, AnimalType, User, UserType
 from sqlalchemy import or_
+
+animals_bp = Blueprint('animals', __name__)
