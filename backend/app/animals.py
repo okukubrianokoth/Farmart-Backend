ad5e60d6b7c4eeaa1966546ef3aa9cf6a5420785
HUSSEIN-IGM <HUSSEINNGUFU205@gmail.com>
2025-10-29 21:58:27 +0300

    GET

--- backend/app/animals.py
+++ backend/app/animals.py
@@ -7,3 +7,8 @@ from sqlalchemy import or_
 animals_bp = Blueprint('animals', __name__)
 
 @animals_bp.route('/animals', methods=['GET'])
+def get_animals():
+    animal_type = request.args.get('type')
+    breed = request.args.get('breed')
+    min_age = request.args.get('min_age')
+    max_age = request.args.get('max_age')
