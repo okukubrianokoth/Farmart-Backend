eb256c590a1a9e233215972b9ee66cb2e8c34dbc
HUSSEIN-IGM <HUSSEINNGUFU205@gmail.com>
2025-10-29 21:41:01 +0300

    imports

---
+++ backend/app/animals.py
@@ -0,0 +1,5 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app import db
+from app.models import Animal, AnimalType, User, UserType
+from sqlalchemy import or_
