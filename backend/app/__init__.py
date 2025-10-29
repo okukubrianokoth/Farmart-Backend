b257b491ab15f1fda4316a034e5fc9f4b920687d
HUSSEIN-IGM <HUSSEINNGUFU205@gmail.com>
2025-10-29 20:53:59 +0300

    extensions

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,3 +4,10 @@ from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
+
+# Initialize extensions
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+cors = CORS()
+bcrypt = Bcrypt()
