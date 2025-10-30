4d1752beb805dced4d2ce728884707284a2d75ed
HUSSEIN-IGM <HUSSEINNGUFU205@gmail.com>
2025-10-30 09:49:32 +0300

    Add AnimalTestCase setup

--- backend/tests/test_animals.py
+++ backend/tests/test_animals.py
@@ -1,3 +1,11 @@
 import unittest
 from app import create_app, db
 from app.models import User, Animal, UserType, AnimalType
+
+class AnimalTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        self.client = self.app.test_client()
