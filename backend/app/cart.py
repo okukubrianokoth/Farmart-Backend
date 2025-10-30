52e83770896396cb0d507d9fbb1dd860ada279d1
HUSSEIN-IGM <HUSSEINNGUFU205@gmail.com>
2025-10-30 08:26:32 +0300

    cart bp

--- backend/app/cart.py
+++ backend/app/cart.py
@@ -2,3 +2,5 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app import db
 from app.models import CartItem, Animal, User
+
+cart_bp = Blueprint('cart', __name__)
