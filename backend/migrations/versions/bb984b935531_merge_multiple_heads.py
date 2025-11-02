c8104dd901a238e422a7119121f2bddc239ccce9
EdwardSnug <prezokams@gmail.com>
2025-11-02 10:28:57 +0300

    Merge Alembic heads for CI

---
+++ backend/migrations/versions/bb984b935531_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""Merge multiple heads
+
+Revision ID: bb984b935531
+Revises: 
+Create Date: 2025-11-02 10:28:03.576890
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bb984b935531'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
